869b8f492bdf0af2fdfbac0946b152e56881a149
Shreya Singh <shreyasinghin24@gmail.com>
2025-08-11 13:32:09 +0000

    c

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+  root.render(
   <React.StrictMode>
    {    }
    <ChakraProvider>
     <App />
    </ChakraProvider>
   </React.StrictMode>
-)
+);
